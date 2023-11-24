5ce53fac1d08638532fcf3bb928f941486469125
David Jeong <46690141+davidtjeong@users.noreply.github.com>
2023-11-24 12:46:15 -0800

    trying out a Spotify Wrapped css template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+  
+  <div class="screen">
+    <div class="dcard">
+    <div class="trigger"></div>
+    <div class="trigger"></div>
+    <div class="trigger"></div>
+    <div class="trigger"></div>
+    <div class="trigger"></div>
+    <div class="trigger"></div>
+    <div class="trigger"></div>
+    <div class="trigger"></div>
+    <div class="trigger"></div>
+    
+<div class="card">
+  <div class="frame">
+    <p>Welcome to the</p>
+    <p>end of a decade.</p>
+    <p>Ready for your</p>
+    <p>2019 Wrapped?</p>
+    
+    <p class="underline">Relive and discover the artists, music and podcasts you've loved the most.</p>
+   
+    
+  </div>
+  
+  
+  
+</div>
     </div>
+   </div>
+  
+  
+   <div class="buttons">
+      <a class="btn" href="https://codepen.io/sirnightowl/" target="_blank">See other Pens</a>
+      <a class="btn btn-ghost" href="https://twitter.com/sirnightowl" target="_blank">Follow on Twitter</a>
+      
+    </div>
+</div>
+
   );
 }
 
